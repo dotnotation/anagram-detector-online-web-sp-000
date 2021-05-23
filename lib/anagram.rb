265d0ec8faef7dbde6f9379d4f1581b4e37d7a01
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-23 21:10:06 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,8 +5,8 @@ class Anagram
     @word = word
   end
   
-  def match(word_array)
-    word_array.select do |word|
+  def match(%w(word))
+    %w(word).select do |word|
       word.split("").sort == @word.split("").sort
     end
   end
